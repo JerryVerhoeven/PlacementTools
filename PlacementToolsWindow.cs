6664386f29184629d1dda73ecd275449a8fb7b6d
Jerry Verhoeven <94966678+JerryVerhoeven@users.noreply.github.com>
2021-11-24 14:45:31 +0100

    Add comment headers.

--- PlacementToolsWindow.cs
+++ PlacementToolsWindow.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿/*
+ * Copyright (C) 2021 Jerry Verhoeven - All Rights Reserved
+ * You may use, distribute and modify this code under the terms of the MIT license.
+ */
+
+using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 using System.Linq;
